535b5fbc1145ea75cdad90fdf6d328d8fb54dcd9
Edward <nipetrovskiy@gmail.com>
2023-05-11 14:59:13 +0500

    second function and docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,13 @@
 def upper():
     word = input("Введите слово")
     print(word.upper())
+
+def capitalineachword():
+    s = input("Введите строку")
+    words = s.split()
+    result = ' '.join(word.capitalize() for word in words)
+    print(result)
+
+'''
+Docstring for second function
+'''
